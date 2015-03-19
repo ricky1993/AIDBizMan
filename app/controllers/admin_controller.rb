d3cf449a1b5af6c5ac8a01de420ac3eae4a1f7b2
ricky1993 <283158956@qq.com>
2015-03-19 15:15:41 +0800

    UpdateAdmin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,9 @@
+class AdminController < ApplicationController
+  def users
+    @users=User.all.page(params[:page])
+  end
+
+  def apply_records
+    @apply_records=ApplyRecord.all.page(params[:page]).order(:created_at=>:desc)
+  end
+end
