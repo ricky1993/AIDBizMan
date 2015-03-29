50376aa56cff0beee49592891d3e06a0bacf91ad
ricky1993 <283158956@qq.com>
2015-03-29 21:10:17 +0800

    latest

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable,:lockable,:authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable,:lockable,:authentication_keys => [:login]
   has_many :message,dependent: :destroy
   attr_accessor :login
   paginates_per 10
